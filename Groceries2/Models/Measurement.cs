c36b8e92c00cfa577745afdeae0fe2fbf92a3ea1
Alex VanLaningham <77026821+ajvanlaningham@users.noreply.github.com>
2022-01-25 10:12:13 -0500

    initil commit, creating the first model

---
+++ Groceries2/Models/Measurement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Groceries2.Models
+{
+    public class Measurement
+    {
+        public int Id { get; set; }
+
+        public string Unit { get; set; }
+    }
+}
