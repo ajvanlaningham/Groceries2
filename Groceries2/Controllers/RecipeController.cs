594237ebe1bde42dcb6ac3b4031f8ba38179463d
Alex VanLaningham <77026821+ajvanlaningham@users.noreply.github.com>
2022-01-30 14:56:57 -0500

    tacked on icon styling, created a recipe controller, created a recipe index view

---
+++ Groceries2/Controllers/RecipeController.cs
@@ -0,0 +1,26 @@
+﻿using Groceries2.Data;
+using Groceries2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Groceries2.Controllers
+{
+    public class RecipeController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public RecipeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Recipe> objRecipeList = _db.Recipies.ToList();
+            return View(objRecipeList);
+        }
+    }
+}
