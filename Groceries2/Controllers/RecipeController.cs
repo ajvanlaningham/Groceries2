f13483de1e45e9526b0b63e20aaf1d05327737e5
Alex VanLaningham <77026821+ajvanlaningham@users.noreply.github.com>
2022-01-31 14:09:12 -0500

    messing around with edit funcitonality in recipes index

--- Groceries2/Controllers/RecipeController.cs
+++ Groceries2/Controllers/RecipeController.cs
@@ -34,6 +34,7 @@ namespace Groceries2.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Recipe obj)
         {
+           
             if(ModelState.IsValid)
             {
                 _db.Recipies.Add(obj);
@@ -41,6 +42,42 @@ namespace Groceries2.Controllers
                 return RedirectToAction("Index");
             }
             
+            return View(obj);
+
+        }
+
+        //get
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var RecipeFromDb = _db.Recipies.Find(id);
+            //var RecipeFromDbFirst = _db.Recipies.FirstOrDefault(i => i.Id == Id);
+            //var RecipeFromDbSingle = _db.Recipies.SingleOrDefault(i => i.Id == Id);
+
+            if (RecipeFromDb == null)
+            {
+                return NotFound();
+            }
+                
+                
+            return View(RecipeFromDb);
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Recipe obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Recipies.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
             return View(obj);
 
         }
